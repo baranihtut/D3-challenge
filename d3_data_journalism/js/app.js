9cb6d9eda60f19141b77853bc590dd1f886ea994
Barani Htut <barani.t16@gmail.com>
2019-11-06 23:25:39 -0800

    Submit

---
+++ d3_data_journalism/js/app.js
@@ -0,0 +1,15 @@
+
+var width = parseInt(d3.select("#scatter").style("width"));
+
+var height = width - width / 3.9;
+
+var margin = 20;
+
+var labelArea = 110;
+
+var tPadBot = 40;
+var tPadLeft = 40;
+
+    d3
+  
+}
